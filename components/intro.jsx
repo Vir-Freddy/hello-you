fa3f6ff26ababbeb0529d9141d75832dad203b39
Vir-Freddy <38891527+Vir-Freddy@users.noreply.github.com>
2020-12-14 21:25:51 -0500

    working one pager rendering react page

---
+++ components/intro.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default () => (
+  <>
+    <div className="name">Freddy Kangai</div>
+    <div className="introduction">
+      <p>
+        I am a Network Admin, Systems admin that has taken an interest in DevOps. Being able to understand the things you're supporting entices me to this point.
+		Hoping I get good with enough practice; hands on. I think I can make a difference in making sure tings are operations on the back end and front end as well.
+      </p>
+      <p>
+        I have held positions as a Support Engineer, Network Administrator, Systems Administrator, Site Support Engineer and so forth.
+      </p>
+    </div>
+  </>
+)
